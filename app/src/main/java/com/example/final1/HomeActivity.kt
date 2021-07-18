8625f4bbbff9d7ad930f701dc226213af3125d06
PanTsu內褲 <supermice0000@gmail.com>
2021-07-18 19:18:55 +0800

    new activity_home.xml,new activity_register.xml,small change to colors.xml !

---
+++ app/src/main/java/com/example/final1/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.final1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn : Button = findViewById(R.id.button)
+        val test=5
+        //test
+
+    }
+}
