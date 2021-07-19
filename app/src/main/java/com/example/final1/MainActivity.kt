2645e85aa30a8f088f0183d60f407e994d9fa46c
黃書堯 <allen10690@gmail.com>
2021-07-19 16:17:50 +0800

    add chital image

--- app/src/main/java/com/example/final1/MainActivity.kt
+++ app/src/main/java/com/example/final1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +12,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btn : Button = findViewById(R.id.button)
         val btn2 : Button = findViewById(R.id.button2)
+        val text :TextView = findViewById(R.id.textView)
         btn2.setOnClickListener{
             val intent = Intent(this@MainActivity, RegisterActivity::class.java)
             startActivity(intent)
+
 
         }
 
