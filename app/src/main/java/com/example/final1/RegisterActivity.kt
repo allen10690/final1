f10cd3ac941b479d1b14a6cd6611d8b68bcb5b31
gangannn <tingchien0205@gmail.com>
2021-08-01 20:46:14 +0800

    build.gradle連接firebase

--- app/src/main/java/com/example/final1/RegisterActivity.kt
+++ app/src/main/java/com/example/final1/RegisterActivity.kt
@@ -1,13 +1,14 @@
 package com.example.final1
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         val btnlogin : Button = findViewById(R.id.button_to_login)
