d8f3582411b0c20360edbaf7f921d7ef2c7e33f5
PanTsu內褲 <supermice0000@gmail.com>
2021-08-14 15:46:18 +0800

    DELETE ALL!

--- app/src/main/java/com/example/final1/User.kt
+++
@@ -1,5 +0,0 @@
-package com.example.final1
-
-data class User(val UserEmail : String? = null, val UserName : String? = null, val UserPhone : String? = null, val currentGroup : String? = null) {
-
-}
